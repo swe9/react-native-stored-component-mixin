3408e24a7afb20cdc1e87ce4464a9f3e29aaf049
Steve Elmer <steve@summergrand.com>
2016-07-26 08:53:54 -0700

    Redo the self references in restore()
    Now for the correct file...

--- storedComponent.js
+++ storedComponent.js
@@ -1,122 +1,153 @@
+// @flow
+
 import React, { Component } from 'react';
-import {
-  Modal,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import Button from 'react-native-button';
-import Icon from 'react-native-vector-icons/Entypo';
-import Logging from './logging.js';
+import { AsyncStorage } from 'react-native';
 
-class ModalHelp extends Component {
+export default StoredComponentMixin = (superclass) => class extends superclass {
   constructor(props) {
     super(props);
-    this.state = { visible: false }
-    this.actions = [{title: 'Done', show: 'always', iconName: 'cross', iconSize: 24}];
+    this.keyPrefix = props.storedName || this.constructor.name;
+    this.keyPrefix = '@' + this.keyPrefix + ":";
+    this.keyListKey = this.keyPrefix + "keyList";
+    this.keyList = [];
   }
 
-  toggle() {
-    this.setState({visible: !this.state.visible});
-  }
-
-  onActionSelected(position) {
-    Logging.log("ModalHelp.onActionSelected", position);
-    if (this.actions[position].title === 'Done') {
-      this.toggle();
+  async reset(list) {
+    if (!list) {
+      list = await this.get(this.keyListKey) || [];
     }
+
+    console.log("Resetting Storage for: ", list);
+    var queue = [];
+    list.forEach((key) => {
+      queue.push(AsyncStorage.removeItem(this.keyPrefix + key), (error)=>{ console.log("reset error", error); });
+    });
+
+    key = this.keyListKey;
+    queue.push(AsyncStorage.removeItem(key));
+
+    await Promise.all(queue);
   }
 
-  render() {
-    return (
-      <Modal transparent={false} visible={this.state.visible} onRequestClose={this.toggle.bind(this)}>
-        <Icon.ToolbarAndroid style={{backgroundColor: '#ddd', height: 56}}
-          navIconName="chevron-thin-left"
-          title="Help"
-          actions={this.actions}
-          onIconClicked={this.toggle.bind(this)}
-          onActionSelected={this.onActionSelected.bind(this)}
-        />
-        <View style={{flex:1,margin:10}}>
-          <Text>
-            Each quote is divided into 4 rows and the letters in each column are then scrambled.
-            Swap the letters up and down in each column until the quote appears.
-            Words may be divided in the middle and continued on the next line, so look closely.
-          </Text>
-          <Text></Text>
-          <Text>
-            To swap letters, just click on them. The first click will highlight a letter. 
-            The second click will swap them.
-          </Text>
-        </View>
-        <View style={{flex:1,margin:10}}>
-          <Text>
-            Puzzle packs may be purchased and a set of puzzles will then be permanently available on 
-            your device.  Restore purchases...
-          </Text>
-        </View>
-      </Modal>
-    );
-  }
-}
+  /* Use addKey if a storage backed state is needed but it's not practical to provide
+     a default to restore() for it.  Case in point, dynamically allocated objects only
+     known at run-time.  Once in the keyList, these objects will be stored just like 
+     any others.  (see this.setState)
+  */ 
+  async addKey(key) {
+    if (this.keyList.includes(key)) { return }
 
-class ModalDebug extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { visible: false }
-    this.actions = [{title: 'Done', show: 'always', iconName: 'cross', iconSize: 24}];
+    this.keyList.push(key);
+    return this.put(this.keyListKey, this.keyList);
   }
 
-  toggle() {
-    this.setState({visible: !this.state.visible});
-  }
+  /* restore() brings back the values of storage backed state.  Anything in
+     storage will have precedence over the defaults.  Essentially, the default
+     is the initializer only.  Adding new defaults will cause the storage to
+     be added the next time restore() is executed on that Component.
+  */
+  async restore(defaults) {
+    console.log("restore", defaults);
+    this.keyList = await this.get(this.keyListKey) || [];
+    var queue = [];
+    var newState = {};
+    var keysUpdated = false;
+    var self = this;
 
-  onActionSelected(position) {
-    Logging.log("ModalDebug.onActionSelected", position);
-    if (this.actions[position].title === 'Done') {
-      this.toggle();
+    var restoreFromDefault = async function(key, def) {
+      self.keyList.push(key);
+      keysUpdated = true;
+
+      if (typeof def === 'function') {
+        return def()
+          .then((value) => {
+            queue.push(self.put(self.keyPrefix + key, value));
+            newState[key] = value;
+          });
+      } else {
+        queue.push(self.put(self.keyPrefix + key, def));
+        newState[key] = def;
+      }
+    };
+
+    var restoreFromStorage = async function(key, def) {
+      return self.get(self.keyPrefix + key)
+        .then((value)=>{
+          if (value === null)
+            return restoreFromDefault(key, def);
+
+          newState[key] = value;
+        });
     }
+
+    this.keyList.forEach((key, index) => {
+      queue.push(restoreFromStorage(key, defaults[key]));
+      delete defaults[key];
+    });
+
+    var newKeys = Object.keys(defaults);
+    newKeys.forEach((key, index) => {
+      queue.push(restoreFromDefault(key, defaults[key]));
+    });
+
+    return Promise.all(queue)
+      .then(() => {
+        queue = [];
+        if (keysUpdated)
+          queue.push(this.put(this.keyListKey, this.keyList));
+        if (newState !== {})
+          queue.push(super.setState(newState));
+        return Promise.all(queue);
+      });
   }
 
-  render() {
-    return (
-      <Modal transparent={false} visible={this.state.visible} onRequestClose={this.toggle.bind(this)}>
-        <Icon.ToolbarAndroid style={{backgroundColor: '#ddd', height: 56}}
-          navIconName="chevron-thin-left"
-          title="Debug"
-          actions={this.actions}
-          onIconClicked={this.toggle.bind(this)}
-          onActionSelected={this.onActionSelected.bind(this)}
-        />
-        <View style={{flex:1,margin:10}}>
-          <Button 
-            containerStyle={[styles.button,styles.buttonContainer]}
-            style={styles.buttonText}
-            onPress={this.props.resetStorage}
-            >
-            Reset Storage
-          </Button>
-        </View>
-      </Modal>
-    );
+  /* The mixin's setState implementation sits between the Component and the
+     react implementation of setState.  The Component gets transparent support
+     for storage backing once it's configured to use this mixin.  Only objects
+     in this.keyList will be stored, all other state is simply forwarded to 
+     the super.
+  */
+  async setState(obj) {
+    var objKeys = Object.keys(obj);
+    var queue = []
+    objKeys.forEach((key, index) => {
+      if (this.keyList.includes(key)) {
+        queue.push(
+          this.put(this.keyPrefix + key, obj[key])
+        );
+      }
+    });
+
+    queue.push(super.setState(obj));
+    return Promise.all(queue);
   }
+
+  /* helper function to get values out of AsyncStorage
+  */
+  async get(key) {
+    try {
+      let value = await AsyncStorage.getItem(key);
+      return JSON.parse(value);
+    } catch (error) {
+      console.log("Retrieval Error: ", error, ' for ', key);
+    }
+    console.log("get failed for ", key);
+    return null;
+  }
+
+  /* helper function to put values into AsyncStorage
+  */
+  async put(key, value) {
+    try {
+      if (typeof value !== 'string') {
+        value = JSON.stringify(value);
+      }
+      await AsyncStorage.setItem(key, value);
+    } catch (error) {
+      console.log("Storage Error: ", error);
+      return false;
+    }
+    return true;
+  }
+
 }
-
-const styles = StyleSheet.create({
-  button: {
-    margin: 2,
-    height:40
-  },
-  buttonContainer: {
-    padding:5, 
-    overflow:'hidden', 
-    borderRadius:10, 
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    fontSize: 24, 
-  },
-});
-
-export { ModalHelp, ModalDebug }
